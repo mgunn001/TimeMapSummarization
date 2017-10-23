994e75de039ba38d89735ed919d1934fc311c662
mgunn001 <mgunnam@cs.odu.edu>
2017-10-23 10:42:36 -0400

    Ready for the initial demo, with HDT being 4

--- timeline.js
+++ timeline.js
@@ -558,8 +558,10 @@
         }
     });
     $(function(){
-         var curJSONFileName= "timemapsumjson_"+collectionsList[parseInt(location.search.split("=")[1])-1].replace(/[^a-z0-9]/gi, '').toLowerCase();
-		$.get(curJSONFileName+'.json',function(response) {
+
+        var curJSONFileName= "timemapsumjson_"+collectionsList[parseInt(location.search.split("=")[1])-1].replace(/[^a-z0-9]/gi, '').toLowerCase();
+		$(".collection_name").html(collectionsList[parseInt(location.search.split("=")[1])-1]);
+        $.get(curJSONFileName+'.json',function(response) {
 			console.log(response);
 			var timelineData = response;
 			window.timeline = new Timeline(timelineData);
